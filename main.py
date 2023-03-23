688bf4e52370b66464cf5e4f81efdca396cb482d
Badmajor <vtk88@mail.ru>
2023-03-23 19:49:51 +0300

    webhook

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, Router
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
+from aiohttp import web
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_PORT, WEBAPP_HOST
 from handlers import register_routers
 
 logging.basicConfig(level=logging.INFO)
@@ -18,8 +20,27 @@ async def main():
     dp = Dispatcher()
     dp.include_router(router)
 
-    await bot.delete_webhook()
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    aiohttp_logger = logging.getLogger("aiohttp.access")
+    aiohttp_logger.setLevel(logging.CRITICAL)
+
+    # Setting webhook
+    await bot.set_webhook(
+        url=WEBHOOK_URL,
+        drop_pending_updates=True,
+        allowed_updates=dp.resolve_used_update_types()
+    )
+
+    # Creating an aiohttp application
+    app = web.Application()
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host=WEBAPP_HOST, port=WEBAPP_PORT)
+    await site.start()
+
+    # Running it forever
+    await asyncio.Event().wait()
+    await bot.session.close()
 
 
 if __name__ == "__main__":
