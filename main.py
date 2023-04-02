473cf3b21dcf788ede87d317d21e9dcadaae8e6d
Badmajor <vtk88@mail.ru>
2023-04-02 11:02:12 +0300

    webhook

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, Router
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
+from aiohttp import web
 
-from config import BOT_TOKEN, WEBHOOK_URL, WEBHOOK_SSL_CERT, WEBAPP_HOST
+from config import BOT_TOKEN, WEBHOOK_URL, WEBHOOK_SSL_CERT, WEBAPP_HOST, WEBHOOK_PATH, WEBAPP_PORT
 from handlers import register_routers
 
 logging.basicConfig(level=logging.INFO)
@@ -18,21 +20,31 @@ async def main():
     dp = Dispatcher()
     dp.include_router(router)
 
-    aiohttp_logger = logging.getLogger("aiohttp.access")
-    aiohttp_logger.setLevel(logging.CRITICAL)
+    try:
+        # Suppress aiohttp access log completely
+        aiohttp_logger = logging.getLogger("aiohttp.access")
+        aiohttp_logger.setLevel(logging.CRITICAL)
 
-    # Setting webhook
-    await bot.set_webhook(
-        url=WEBHOOK_URL,
-        certificate=WEBHOOK_SSL_CERT,
-        ip_address=WEBAPP_HOST,
-        drop_pending_updates=True,
-        allowed_updates=dp.resolve_used_update_types()
-    )
+        # Setting webhook
+        await bot.set_webhook(
+            url=WEBHOOK_URL + WEBHOOK_PATH,
+            certificate=WEBHOOK_SSL_CERT,
+            drop_pending_updates=True,
+            allowed_updates=dp.resolve_used_update_types()
+        )
 
-    # Running it forever
-    await asyncio.Event().wait()
-    await bot.session.close()
+        # Creating an aiohttp application
+        app = web.Application()
+        SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, host=WEBAPP_HOST, port=WEBAPP_PORT)
+        await site.start()
+
+        # Running it forever
+        await asyncio.Event().wait()
+    finally:
+        await bot.session.close()
 
 
 if __name__ == "__main__":
